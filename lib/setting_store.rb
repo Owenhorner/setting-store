261c2690272c007c27d9fb3ea61f2313c734842e
Owen Horner <owenhorner@live.com.au>
2016-11-26 09:42:06 +1100

    adds setting store module

--- lib/setting_store.rb
+++ lib/setting_store.rb
@@ -1,5 +1,30 @@
 require "setting_store/version"
+require 'pstore'
 
 module SettingStore
-  # Your code goes here...
+  def self.set(field, value: nil)
+    store.transaction { store[field.to_sym] = value }
+  end
+
+  def self.get(field)
+    store.transaction { store[field.to_sym] }
+  end
+
+  def self.active?(field)
+    get(field.to_sym) == true
+  end
+
+  private
+
+  def self.store
+    @store ||= PStore.new(store_filename)
+  end
+
+  def self.store_filename
+    if ENV['STORE_FILENAME']
+      ENV['STORE_FILENAME']
+    else
+      "local_settings_store.pstore"
+    end
+  end
 end
--- spec/setting_store_spec.rb
+++ spec/setting_store_spec.rb
@@ -5,7 +5,37 @@ describe SettingStore do
     expect(SettingStore::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#set" do
+    it "sets a value to store" do
+      expect(SettingStore.set(:example, value: true)).to eq(true)
+    end
+  end
+
+  describe "#get" do
+    it "gets a settings from the store" do
+      SettingStore.set(:get_example, value: "hello")
+      expect(SettingStore.get(:get_example)).to eq("hello")
+    end
+
+    it "gets nil for non-existent field" do
+      expect(SettingStore.get(:no_example)).to eq(nil)
+    end
+  end
+
+  describe "#active?" do
+    it "gets a truthy value from the store" do
+      SettingStore.set(:active_feature, value: true)
+      expect(SettingStore.active?(:active_feature)).to eq(true)
+    end
+
+    it "gets a falsey value whenever flag is false" do
+      SettingStore.set(:inactive_feature, value: false)
+      expect(SettingStore.active?(:inactive_feature)).to eq(false)
+    end
+
+    it "gets a falsey value whenever a string is returned from flag" do
+      SettingStore.set(:other_feature, value: "pen-pineapple")
+      expect(SettingStore.active?(:other_feature)).to eq(false)
+    end
   end
 end
